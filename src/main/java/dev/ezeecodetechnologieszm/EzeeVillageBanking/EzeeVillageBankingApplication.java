92832da44aa3e8143401e11bdd0855c12517251f
Kaunda Kabwe <kaunda.ezeecode@outlook.com>
2025-01-30 09:33:35 +0200

    Initial project - spring boot

---
+++ src/main/java/dev/ezeecodetechnologieszm/EzeeVillageBanking/EzeeVillageBankingApplication.java
@@ -0,0 +1,13 @@
+package dev.ezeecodetechnologieszm.EzeeVillageBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzeeVillageBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzeeVillageBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ezeecodetechnologieszm/EzeeVillageBanking/EzeeVillageBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ezeecodetechnologieszm.EzeeVillageBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzeeVillageBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
